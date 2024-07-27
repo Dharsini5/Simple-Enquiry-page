266a5c6aa9901163236d75b26a4f766dad09b741
Dharsini5 <138126680+Dharsini5@users.noreply.github.com>
2024-07-27 19:05:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Enquiry</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<button class="whatsapp"><a href="https://api.whatsapp.com/send?phone=+91 7200040053&amp;text=Hi Vetrii Foods, " target="_blank">
+    <img src="whatsapp.png" height="100%" width="100%"></a>
+</button>
+<div class="vt">
+	<div class="logo">
+		<a href="index.php">
+		<img src="vetrii-logo.png" alt="logo"  height="100%" width="100%">
+		</a>
+	</div>
+	<div class="nav">
+		<ul class="list">
+			<li>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Our Products</a></li>
+				<li><a href="#">Gallery</a></li>
+				<li><a href="#">Testimonial</a></li>
+				<li><a href="#">Enquiry</a></li>
+				<li><a href="#">Contact us</a></li>
+			</li>
+		</ul>	
+
+</div>
+</div>
+<div class="about">
+		<h1>Enquiry</h1>
+		<h3><a href="abt">Home|Enquiry</a>
+		</h3>
+</div>
+<div class="r">
+<div class="container">
+			<div class="row">
+			<div><label for="exampleFormControlInput1" class="form-label">Name</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1"></div>
+			<div class="col-3"><label for="exampleFormControlTextarea1" class="form-label">Email</label>
+  <input class="form-control" id="exampleFormControlTextarea1" ></div>
+		</div>
+		<div class="row">
+			<div class="col-3"><label>Select product</label><select class="form-select" aria-label="Default select example">
+  <option selected>Select product</option>
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+</select></div>
+			<div><label for="exampleFormControlInput1" class="form-label">Name</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1"></div>
+		</div>
+		<div class="row">
+		<div class="col-6">  <label for="exampleFormControlTextarea1" class="form-label"></label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Message"></textarea></div>
+</div>
+<div class="row">
+	<div class="col-6">
+	<button type="button" class="btn btn-primary btn-lg">Submit</button></div>
+</div>
+		</div>
+		<div class="grl">
+		<a href="index.php">
+		<img src="enquiry.png" alt="grl"  height="100%" width="100%">
+		</a>
+	</div>
+	</div>
+	<div class="info">
+		<div class="row" style="color:white;">
+			<div class="col-3"><div class="log">
+		<a href="index.php">
+		<img src="vetrii-logo.png" alt="log"  height="100%" width="100%">
+		</a>
+	</div>
+				Committed to delivering authentic taste, our ISO-certified brand crafts a diverse range of Agro Food and FMCG products under the name VETRII, ensuring quality and satisfaction in every bite.
+			</div>
+			<div class="col-3" id="link" style="color:white;">
+				<h3>Link</h3>
+			<ul>
+				<li>Home</li>
+				<li>About Us</li>
+				<li>Our Product</li>
+				<li>Gallery</li>
+				<li>Testimonial</li>
+				<li>Enquiry</li>
+				<li>Contact Us</li>
+			</ul>
+		</div>
+		<div class="col-3" id="contact" style="color:white;">
+			<h3>Contact info</h3>
+			<ul>
+				<li>825-826,Rangai Gounder Street,<br>Coimbatore – 641 001</li>
+				<li>0422 2392412<br>+91 73737 66778</li>
+				<li>vetriifoods@gmail.com</li>
+			</ul>
+		</div> 
+		<div class="col-3" style="color:white;">
+			<h3>Map</h3>
+			<img src="map.png">
+		</div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
